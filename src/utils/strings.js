0ebe4728bed141ad43d81a096dfdc156f028d12e
Noël Macé <contact@noelmace.com>
2016-05-08 21:48:08 +0200

    fix syntax error in strings.js

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,3 +1,3 @@
 
 export let camelToDash = (string) => string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
-export let dashToCamel = (string) => string..replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
+export let dashToCamel = (string) => string.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
