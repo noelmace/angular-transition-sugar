9e866f35febeb9fd3b8f105063e724ba66665c03
Noel Mace <contact@noelmace.com>
2016-05-10 13:39:55 +0200

    add default Module config values and templateDependencies condition param

--- src/Module.js
+++ src/Module.js
@@ -4,27 +4,27 @@ import { lazyLoadConfig } from './utils/routes';
 import * as MainModuleUtil from './utils/mainModule';
 import * as TemplateUtil from './utils/templates';
 
-let addTemplateModule = (appModule, templateUrl) => {
-    if (templateUrl) {
+let addTemplateModule = (appModule, templateUrl, templatesDependencies) => {
+    if (templatesDependencies && templateUrl) {
         appModule.requires.push(templateUrl);
     }
 }
 
-export function Module({ name, dependencies, configs, main, debug, html5mode }) {
+export function Module({ name, dependencies = [], configs = [], main = false, debug = false, html5mode = false, templatesDependencies = true }) {
     return function decorator(component) {
         let appModule = angular.module(name, dependencies);
 
-        addTemplateModule(appModule, component.$kissDecoratorsConfig.componentConfig.templateUrl);
+        addTemplateModule(appModule, component.$kissDecoratorsConfig.componentConfig.templateUrl, templatesDependencies);
         appModule.component(component.$kissDecoratorsConfig.name, component.$kissDecoratorsConfig.componentConfig);
 
         if (component.$kissDecoratorsConfig.dependencies) {
             if (component.$kissDecoratorsConfig.dependencies.directives) {
                 component.$kissDecoratorsConfig.dependencies.directives.forEach((directive) => {
                     if (angular.isDefined(directive.$kissDecoratorsConfig.componentConfig)) {
-                        addTemplateModule(appModule, directive.$kissDecoratorsConfig.componentConfig.templateUrl);
+                        addTemplateModule(appModule, directive.$kissDecoratorsConfig.componentConfig.templateUrl, templatesDependencies);
                         appModule.component(directive.$kissDecoratorsConfig.name, directive.$kissDecoratorsConfig.componentConfig);
                     } else if (angular.isDefined(directive.$kissDecoratorsConfig.directiveConfig)) {
-                        addTemplateModule(appModule, directive.$kissDecoratorsConfig.directiveConfig.templateUrl);
+                        addTemplateModule(appModule, directive.$kissDecoratorsConfig.directiveConfig.templateUrl, templatesDependencies);
                         appModule.directive(directive.$kissDecoratorsConfig.name, directive.$kissDecoratorsConfig.directiveConfig);
                     }
                 });
