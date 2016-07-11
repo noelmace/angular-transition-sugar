0b23101f034abecd3164e1dc3558423756cac59a
Noel Mace <contact@noelmace.com>
2016-07-11 13:11:26 +0200

    fix Injectable error on minified js files

--- src/core-decorators/Injectable.js
+++ src/core-decorators/Injectable.js
@@ -2,6 +2,6 @@ import { upperToLowerCamel } from './utils/strings';
 
 export function Injectable(config) {
     return function decorator(ctrl) {
-        ctrl.$kissDecoratorsConfig = { injectableId: upperToLowerCamel(ctrl.name) };
+        ctrl.$kissDecoratorsConfig = { injectableId: config.id || upperToLowerCamel(ctrl.name) };
     }
 }
